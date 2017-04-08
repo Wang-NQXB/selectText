8106e71b8502da426c6b822686a8517958a8a87f
Wang-NQXB <hust_wangyue@foxmail.com>
2017-04-08 15:21:38 +0800

    Update sortObj.js

--- sortObj.js
+++ sortObj.js
@@ -1,24 +1,23 @@
 function sortObj(obj) {
     function compareObj(a, b) {
-        try {
-            a = JSON.stringify(a);
-            b = JSON.stringify(b);
-        } catch (e) {}
-
+        try { a = JSON.stringify(a); } catch (e) {}
+        try { b = JSON.stringify(b); } catch (e) {}
         return a > b ? 1 : -1;
     }
 
     let res;
     if (obj instanceof Array) {
-        res = [];
-        for (var item of obj.sort(compareObj)) {
-            res.push(item instanceof Object ? sortObj(item) : item);
+        for (var item of obj) {
+            item = sortObj(item);
         }
+        res = obj.sort(compareObj);
     } else if (obj instanceof Object) {
         res = {};
         for (var key of Object.keys(obj).sort()) {
             res[key] = obj[key] instanceof Object ? sortObj(obj[key]) : obj[key];
         }
+    } else {
+        res = obj;
     }
     return res;
 }
